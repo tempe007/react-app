5359e124b5349f92f7e9c6822b8dc7dbf925aeda
신지호-플그림 <shinjiho@sinjihoui-noteubug-2.local>
2024-06-11 10:24:27 +0900

    //react strict mode 주석

--- src/index.js
+++ src/index.js
@@ -15,3 +15,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
+//react strict mode 주석
+//https://velog.io/@citron03/React%EC%9D%98-StrictMode%EC%97%90-%EB%8C%80%ED%95%B4%EC%84%9C
